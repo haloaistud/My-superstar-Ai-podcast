69d98278bb1dae4eb690351d0eb60e512ecd402b
Timothy Henton <apollolenior@gmail.com>
2025-09-22 16:09:24 +0000

    Male background Black and blue Hue color scheme (_for element <Home>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
+    <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable} dark`}>
       <body>
         <main>{children}</main>
         <Toaster />
