52f3e2144167eb1ca6abdda89750b0f1b81ac7ca
Timothy Henton <apollolenior@gmail.com>
2025-09-26 23:21:52 +0000

    Exported from IDX

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,7 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
+{pkgs}
+: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
